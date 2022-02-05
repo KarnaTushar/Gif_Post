634fd7bbe5a7ee3e2e6191286e01a9230bedb2ea
Tushar <tusharkarna05@gmail.com>
2022-02-05 01:12:27 -0800

    updated placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -142,7 +142,7 @@ function App() {
                 <div class="flex items-center border-b border-indigo-500 py-2">
                   <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
                     type="text"
-                    placeholder="Post URL"
+                    placeholder="Post message"
                     onChange={(e) => setPostMessgae(e.target.value)} value={postMessage}
                   />
                 </div>
